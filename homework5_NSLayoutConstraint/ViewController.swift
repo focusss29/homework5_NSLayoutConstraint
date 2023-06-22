08f677c73f2b4b9f804a118401d97c6056b91d88
Ilya Pogozhev <ilya_pogozhev29@mail.ru>
2023-06-22 18:58:26 +0600

    add image and 2 labels

--- homework5_NSLayoutConstraint/ViewController.swift
+++ homework5_NSLayoutConstraint/ViewController.swift
@@ -26,6 +26,34 @@ class ViewController: UIViewController {
         return view
     }()
     
+    let myImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "book")
+        image.tintColor = .gray
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    let myMainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Литература — это самый приятный способ игнорировать жизнь."
+        label.numberOfLines = 0
+        label.textAlignment = .justified
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let myTextLabel: UILabel = {
+        let label = UILabel()
+        label.text = "''Литература — это волшебное убежище, где мы можем отстраниться от суеты и проблем повседневной жизни. Она приглашает нас в параллельные миры, где можно погрузиться в приключения и романтику, забыв о реальности. Книги позволяют нам скользить по страницам, словно по магическому ковру, и наслаждаться прекрасными историями, созданными умами талантливых авторов. В этом пространстве мы можем познать героев, их страсти и стремления, воспроизвести их радости и печали, не покидая уютное кресло. Литература — это близкий друг, который позволяет нам временно уйти от реальности и насладиться миром фантазии.'"
+        label.numberOfLines = 0
+        label.textAlignment = .justified
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
         setupScene()
@@ -37,6 +65,8 @@ private extension ViewController {
         view.backgroundColor = .white
         view.addSubview(myMainView)
         view.addSubview(circleView)
+        view.addSubview(myImage)
+        view.addSubview(myMainLabel)
     }
     
     func makeConstraints() {
@@ -50,6 +80,22 @@ private extension ViewController {
             circleView.heightAnchor.constraint(equalToConstant: 140),
             circleView.centerXAnchor.constraint(equalTo: myMainView.centerXAnchor),
             circleView.centerYAnchor.constraint(equalTo: myMainView.centerYAnchor, constant: 50)
+        ])
+        NSLayoutConstraint.activate([
+           
+            myImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            myImage.topAnchor.constraint(equalTo: myMainView.bottomAnchor, constant: 75),
+            myImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -298),
+            myImage.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -373),
+            
+            myImage.widthAnchor.constraint(equalToConstant: 70),
+            myImage.heightAnchor.constraint(equalToConstant: 70)
+        ])
+        NSLayoutConstraint.activate([
+            myMainLabel.topAnchor.constraint(equalTo: myMainView.bottomAnchor, constant: 53),
+            myMainLabel.leadingAnchor.constraint(equalTo: myImage.trailingAnchor, constant: 10),
+            myMainLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            myMainLabel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -350)
         ])
     }
 }
