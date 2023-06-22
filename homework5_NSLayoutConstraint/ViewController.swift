371bbfb540c8e832e43cbf683cac0b5fc46dcaaf
Ilya Pogozhev <ilya_pogozhev29@mail.ru>
2023-06-22 17:33:19 +0600

    Add myMainView and circleView + constraints

--- homework5_NSLayoutConstraint/ViewController.swift
+++ homework5_NSLayoutConstraint/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let myMainView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemIndigo
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 25
+        view.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+        return view
+    }()
+    
+    let circleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemOrange
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 70
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupScene()
+        makeConstraints()
     }
-
-
 }
+private extension ViewController {
+    func setupScene() {
+        view.backgroundColor = .white
+        view.addSubview(myMainView)
+        view.addSubview(circleView)
+    }
+    
+    func makeConstraints() {
+        NSLayoutConstraint.activate([
+            myMainView.widthAnchor.constraint(equalToConstant: 393),
+            myMainView.heightAnchor.constraint(equalToConstant: 310),
+            myMainView.topAnchor.constraint(equalTo: view.topAnchor)
+        ])
+        NSLayoutConstraint.activate([
+            circleView.widthAnchor.constraint(equalToConstant: 140),
+            circleView.heightAnchor.constraint(equalToConstant: 140),
+            circleView.centerXAnchor.constraint(equalTo: myMainView.centerXAnchor),
+            circleView.centerYAnchor.constraint(equalTo: myMainView.centerYAnchor, constant: 50)
+        ])
+    }
+}
+
 
