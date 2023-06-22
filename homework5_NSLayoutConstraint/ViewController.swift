0367bd95cc72e8124597b113b406a81957f4980e
Ilya Pogozhev <ilya_pogozhev29@mail.ru>
2023-06-22 16:25:00 +0600

    Initial Commit

---
+++ homework5_NSLayoutConstraint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework5_NSLayoutConstraint
+//
+//  Created by Ilya Pogozhev on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
